7608463494a7b3b255593027a748322c259be3cb
deechael <deechael@outlook.com>
2023-05-14 10:43:12 +0800

    feat: rendering folders

--- src/jvmMain/kotlin/net/deechael/kookdesktop/page/HomePage.kt
+++ src/jvmMain/kotlin/net/deechael/kookdesktop/page/HomePage.kt
@@ -6,8 +6,13 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import io.kamel.image.KamelImage
 import io.kamel.image.lazyPainterResource
@@ -61,10 +66,7 @@ fun Home() {
                             )
                             .width(48.dp)
                             .height(48.dp)
-                            .align(Alignment.CenterHorizontally)
-                            .clickable(true, guild.info.name) {
-                                TODO("Switch guild")
-                            },
+                            .align(Alignment.CenterHorizontally),
                         shape = RoundedCornerShape(10.dp)
                     ) {
                         TooltipArea(tooltip = {
@@ -87,20 +89,118 @@ fun Home() {
                                 modifier = Modifier
                                     .height(48.dp)
                                     .width(48.dp)
+                                    .clickable(true, guild.info.name) {
+                                        TODO("Switch guild")
+                                    }
                             ) {
-                                KamelImage(
-                                    resource = lazyPainterResource(
-                                        data = guild.info.icon
-                                    ),
-                                    contentDescription = guild.info.name
-                                )
+                                if (guild.info.icon != "") {
+                                    KamelImage(
+                                        resource = lazyPainterResource(
+                                            data = guild.info.icon
+                                        ),
+                                        contentDescription = guild.info.name
+                                    )
+                                } else {
+                                    Text(
+                                        text = guild.info.name
+                                    )
+                                }
                             }
                         }
                     }
                 } else if (guild is GuildGroup) {
-                    Text(
-                        text = guild.name
-                    )
+                    var showGuilds by rememberSaveable {
+                        mutableStateOf(false)
+                    }
+
+                    Card(
+                        modifier = Modifier
+                            .padding(
+                                top = 8.dp,
+                                start = 4.dp,
+                                end = 4.dp
+                            )
+                            .width(56.dp)
+                            .requiredWidthIn(
+                                min = 56.dp
+                            )
+                            .align(Alignment.CenterHorizontally),
+                        shape = RoundedCornerShape(10.dp)
+                    ) {
+                        TooltipArea(tooltip = {
+                            Card(
+                                shape = RoundedCornerShape(5.dp)
+                            ) {
+                                Text(
+                                    text = guild.name,
+                                    modifier = Modifier.padding(
+                                        top = 4.dp,
+                                        bottom = 4.dp,
+                                        start = 8.dp,
+                                        end = 8.dp
+                                    )
+                                )
+                            }
+                        }) {
+                            Box(
+                                modifier = Modifier
+                                    .padding(4.dp)
+                                    .width(48.dp)
+                                    .height(48.dp)
+                                    .clickable(true, guild.name) {
+                                        showGuilds = !showGuilds
+                                    }
+                            ) {
+                                Text(
+                                    text = if (guild.name != "") guild.name else "Folder"
+                                )
+                            }
+                        }
+                        if (showGuilds) {
+                            for (subGuild in guild.guilds) {
+                                TooltipArea(tooltip = {
+                                    Card(
+                                        shape = RoundedCornerShape(5.dp)
+                                    ) {
+                                        Text(
+                                            text = subGuild.info.name,
+                                            modifier = Modifier.padding(
+                                                top = 4.dp,
+                                                bottom = 4.dp,
+                                                start = 8.dp,
+                                                end = 8.dp
+                                            )
+                                        )
+                                    }
+                                }) {
+                                    Box(
+                                        contentAlignment = Alignment.Center,
+                                        modifier = Modifier
+                                            .background(Color(guild.color))
+                                            .padding(4.dp)
+                                            .height(48.dp)
+                                            .width(48.dp)
+                                            .clickable(true, subGuild.info.name) {
+                                                TODO("Switch guild")
+                                            }
+                                    ) {
+                                        if (subGuild.info.icon != "") {
+                                            KamelImage(
+                                                resource = lazyPainterResource(
+                                                    data = subGuild.info.icon
+                                                ),
+                                                contentDescription = subGuild.info.name
+                                            )
+                                        } else {
+                                            Text(
+                                                text = subGuild.info.name
+                                            )
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
                 }
             }
         }
